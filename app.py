78e762701852877067fb7a0cd46e7430f1bbc510
ayushpratap12 <ayushpratapsingh2002@gmail.com>
2025-01-30 13:13:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
 
+# def fetch_poster(movie_id):
+#     requests.get('')
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movies_list:
+        movie_id = i[0]
+        #fetching the poster from api
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies
+
+movie_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movie_dict)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+'How would you like to be contacted?',
+movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
